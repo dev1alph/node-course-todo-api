544fb3f6616601c8b851ee099c4c2a971746a8b4
Asim <asim.ibn.asghar@gmail.com>
2017-04-08 22:18:21 +0100

    Create Development and Test DBs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // process.env.MONGODB_URI is for heroku.com
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+require('./config/config.js');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -7,12 +10,13 @@ var {Todo} = require ('./models/todo');
 var {User} = require ('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
 
 
+// INSERT
 app.post('/todos', (req, res) => {
     var todo = new Todo({
        text: req.body.text
@@ -28,6 +32,8 @@ app.post('/todos', (req, res) => {
 
 
 
+
+// SELECT *
 app.get('/todos', (req, res) => {
    Todo.find().then((todos) => {
        res.send({todos});
@@ -38,6 +44,8 @@ app.get('/todos', (req, res) => {
 
 
 
+
+//  SELECT where id = :id
 app.get('/todos/:id', (req, res) => {
    var id = req.params.id;
 
@@ -61,8 +69,9 @@ app.get('/todos/:id', (req, res) => {
 
 
 
-//return null if no item is deleted
-app.delete('/todos/:id', (req, res) => {
+
+// DELETE where id = :id
+app.delete('/todos/:id', (req, res) => {//return null if no item is deleted
     var id = req.params.id;
 
     if(!ObjectID.isValid(id)){
@@ -83,8 +92,44 @@ app.delete('/todos/:id', (req, res) => {
 
 
 
+
+// UPDATE - should use patch route for update *recommended*
+app.patch('/todos/:id', (req, res) => {
+   var id   = req.params.id;
+   var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime(); // Unix timestamp
+    }else{
+        body.completed   = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+           if(!todo) {
+               return res.status(404).send();
+           }
+
+           res.send({todo});
+       }).catch((e) => {
+               res.status(400).send();
+         });
+});
+
+
+
+
+
+
+
+
 app.listen(port, () => {
-    console.log(`Started oup at port ${port}`);
+    console.log(`Started up at port ${port}`);
 });
 
 
