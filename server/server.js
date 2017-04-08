07ce0ea8bba1a9c2dfa15cdcbf43c8298530a57e
Asim <asim.ibn.asghar@gmail.com>
2017-04-08 20:02:37 +0100

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -61,6 +61,28 @@ app.get('/todos/:id', (req, res) => {
 
 
 
+//return null if no item is deleted
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+       if(!todo){
+           return res.status(404).send();
+       }
+
+       res.send(todo);
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+
+
 app.listen(port, () => {
     console.log(`Started oup at port ${port}`);
 });
